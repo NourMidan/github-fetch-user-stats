fb42f493ea48d39e79b7018f565b67fc62321464
NourMidan <58523548+NourMidan@users.noreply.github.com>
2020-08-05 12:28:47 +0200

    Updates

---
+++ precache-manifest.fb4d766f1616bea782aaf0c032a8442c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "755d2bba2c3b8daf0b0f9487fb5d50bc",
+    "url": "/github-fetch-user-stats/index.html"
+  },
+  {
+    "revision": "d3b435fdfd2ff4c69524",
+    "url": "/github-fetch-user-stats/static/css/main.a7531abd.chunk.css"
+  },
+  {
+    "revision": "947dc0be3bd58d6eaefc",
+    "url": "/github-fetch-user-stats/static/js/2.da6793b2.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/github-fetch-user-stats/static/js/2.da6793b2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d3b435fdfd2ff4c69524",
+    "url": "/github-fetch-user-stats/static/js/main.cb79d8ed.chunk.js"
+  },
+  {
+    "revision": "b13dc237102633aff543",
+    "url": "/github-fetch-user-stats/static/js/runtime-main.d722b193.js"
+  }
+]);
